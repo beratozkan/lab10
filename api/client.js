d0dd3ee0472b66ff41864b54fdb52a1c616f9c3e
Berat <berat_54_2014@hotmail.com>
2026-01-20 00:25:22 +0300

    last

--- api/client.js
+++ api/client.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 const API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL;
 console.log(API_BASE_URL);
 export const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: 'http://192.168.1.106:3000',
   timeout: 10000, 
 });
 
